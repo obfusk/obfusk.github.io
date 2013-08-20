a0ccce6eaee427285339b748a26f6a5956d7e45e
Felix C. Stegerman <flx@obfusk.net>
2013-08-21 00:08:36 +0200

    ...

--- Rakefile
+++ Rakefile
@@ -1,7 +1,54 @@
 require 'haml'
 
+$:.unshift File.expand_path('../lib', __FILE__)
+require 'obfusk/vlo'
+
+desc 'new page'
+task :page do                                                   # {{{1
+  name  = ENV['NAME'].to_s  ; raise 'empty NAME' if name.empty?
+  title = ENV['TITLE'].to_s ; title = name if title.empty?
+  f     = "pages/#{name}.md"
+  y     = { 'title' => title, 'order' => -1 } .to_yaml
+  p     = <<-END .gsub(/^ {4}/, '')
+    # My new page
+
+    ...
+  END
+  raise "existing file: #{f}" if File.exists? f
+  Obfusk::VLO.write_file f, "#{y}\n#{p}"
+end                                                             # }}}1
+
+desc 'new post'
+task :post do                                                   # {{{1
+  title = ENV['TITLE'].to_s ; raise 'empty TITLE' if title.empty?
+  cat   = ENV['CAT'].to_s   ; raise 'empty CAT'   if cat.empty?
+  tags  = ENV['TAGS'].to_s.split /[\s,]+/
+  date  = Time.now.strftime '%F_%T'
+  safe  = Obfusk::VLO.safe_title title
+  f     = "posts/#{date}-#{safe}.md"
+  y     = { 'title' => title, 'category' => cat, 'tags' => tags } \
+            .to_yaml
+  p     = <<-END .gsub(/^ {4}/, '')
+    # My new post
+
+    ...
+  END
+  raise "existing file: #{f}" if File.exists? f
+  Obfusk::VLO.write_file f, "#{y}\n#{p}"
+end                                                             # }}}1
+
+desc 'build website'
+task :build do
+  Obfusk::VLO.build
+end
+
+desc 'build website'
+task :clean do
+  sh 'rm -fr html/ index.html'
+end
+
 desc 'serve w/ sinatra'
-task :sinatra do
+task sinatra: :build do
   require 'sinatra'
   set :public_folder, File.expand_path('..', __FILE__)
   get('/') { redirect '/index.html' }
--- lib/obfusk/vlo.rb
+++ lib/obfusk/vlo.rb
@@ -17,25 +17,50 @@ require 'yaml'
 
 module Obfusk; module VLO
 
-  class Pygments < Redcarpet::Render::HTML
+  class RedPyg < Redcarpet::Render::HTML
     def block_code(code, lang)
       Pygments.highlight code, lexer: lang
     end
   end
 
   MD = Redcarpet::Markdown.new(
-    Pygments, no_intra_emphasis: true, fenced_code_blocks: true,
+    RedPyg, no_intra_emphasis: true, fenced_code_blocks: true,
     autolink: true, lax_spacing: true
   )
 
-  Page  = Struct.new :file, :order
-  Post  = Struct.new :file, :dir, :date, :tags, :cat
+  # --
+
+  def self.struct(*keys)
+    s = Struct.new(*keys.map(&:to_sym))
+    s.class_eval do
+      def initialize(h = {})
+        h.each_pair { |k,v| self[k] = v }
+      end
+      def check!
+        each_pair { |k,v| v or raise 'empty!' }
+        self
+      end
+    end
+    s
+  end
+
+  # --
+
+  POSTNAME = /^
+    (?<date> \d\d\d\d - \d\d - \d\d ) _
+    (?<time> \d\d : \d\d : \d\d ) - (?<rest> .* )
+  $/x
+
+  Config  = struct(*%w{ title tagline })
+  Page    = struct(*%w{ hdr md name file order })
+  Post    = struct(*%w{ hdr md name file dir title date tags cat })
 
   # --
 
   def self.build                                                # {{{1
-    c = YAML.load_file 'config.yml'
-    layout            = get_layout :index
+    c = Config.new(YAML.load_file('config.yml'))
+    FileUtils.mkdir_p 'html'
+    layout            = get_layout :layout
     pages             = build_pages c, layout
     posts, cats, tags = build_posts c, layout
     build_archive_page  c, posts
@@ -47,20 +72,20 @@ module Obfusk; module VLO
 
   def self.build_pages(c, layout)                               # {{{1
     ps = Dir['pages/*.md'].sort.map do |p|
-      x = load_page(p, File.read p)
-      File.write "html/#{x.file}", render_page(x, layout)
+      x = load_page(p, File.read(p))
+      write_file "html/#{x.file}", render_page(c, x, layout)
       x
     end.sort_by { |x| x.order }
   end                                                           # }}}1
 
   def self.build_posts(c, layout)                               # {{{1
-    cats = []; tags = []
+    cats = {}; tags = {}
     ps = Dir['posts/*.md'].sort.each do |p|
-      x = load_post(p, File.read p)
-      FileUtils.mkdir_p "html/#{p.dir}"
-      File.write "html/#{x.file}", render_post(x, layout)
+      x = load_post(p, File.read(p))
+      FileUtils.mkdir_p "html/#{x.dir}"
+      write_file "html/#{x.file}", render_post(c, x, layout)
       (cats[x.cat] ||= []) << x
-      x.tags.each |t| do
+      x.tags.each do |t|
         (tags[t] ||= []) << x
       end
       x
@@ -81,46 +106,56 @@ module Obfusk; module VLO
   end
 
   def self.build_index_page(c)
+    # render_page, load_page(
   end
 
   # --
 
-  def self.load_page(p, s)
-    hdr, md, title = load_yaml_md p, s
-    Page.new "#{title}.html", hdr[:order]
-  end
+  def self.load_page(p, s)                                      # {{{1
+    hdr, md, name = load_yaml_md p, s
+    Page.new(
+      hdr: hdr, md: md, name: name, file: "#{name}.html",
+      order: hdr['order']
+    ).check!
+  end                                                           # }}}1
 
   def self.load_post(p, s)                                      # {{{1
-    hdr, md, title  = load_yaml_md p, s
-    cat             = hdr[:category]
-    date            = DateTime.parse hdr[:date]
-    dir             = [cat,date]*'/'
-    file            = "#{[dir,title]*'/'}.html"
-    Post.new file, dir, date, hdr[:tags], cat
+    hdr, md, nm   = load_yaml_md p, s
+    m = nm.match(POSTNAME) or raise "invalid post name: #{nm}"
+    name          = m[:rest]
+    date          = DateTime.parse "#{m[:date]} #{m[:time]}"
+    cat           = hdr['category']
+    dir           = [cat,m[:date]]*'/'
+    file          = "#{[dir,name]*'/'}.html"
+    Post.new(
+      hdr: hdr, md: md, name: name, file: file, dir: dir,
+      title: hdr['title'], date: date, tags: hdr['tags'], cat: cat
+    ).check!
   end                                                           # }}}1
 
   def self.load_yaml_md(p, s)
-    hdr, md = s.split '\n\n', 2
-    title   = File.basename(s, '.md').gsub(/[^A-Za-z0-9_-]/, '_')
-    [hdr, md, title]
+    hdr, md = s.split "\n\n", 2
+    name    = File.basename(p, '.md')
+    [YAML.load(hdr), md, name]
   end
 
   # --
 
-  def self.render_page(p, layout)
-    render_view :page, layout, page: p
+  def self.render_page(c, p, layout)
+    render_view :page, layout, config: c, page: p,
+      html: render_md(p.md)
   end
 
-  def self.render_post(p, layout)
-    render_view :post, layout, post: p
+  def self.render_post(c, p, layout)
+    render_view :post, layout, config: c, post: p,
+      html: render_md(p.md)
   end
 
   # --
 
   def self.render_view(name, layout, locals = {})
-    render_haml layout, locals do
-      render_haml get_haml(File.read("views/#{name}.haml")), locals
-    end
+    p = render_haml get_haml(File.read("views/#{name}.haml")), locals
+    render_haml(layout, locals) { p }
   end
 
   def self.get_layout(name)
@@ -134,11 +169,22 @@ module Obfusk; module VLO
   end
 
   def self.render_haml(h, locals = {}, &b)
-    h.render Object.new locals: locals, &b
+    h.render Object.new, locals, &b
   end
 
   def self.get_haml(s)
     Haml::Engine.new s
+  end
+
+  # --
+
+  def self.write_file(f, s)
+    puts "--> #{f}"
+    File.write f, s
+  end
+
+  def self.safe_title(s)
+    s.gsub /[^A-Za-z0-9_-]/, '_'
   end
 
 end; end
