3495a8eaf712c5a49dc3bdf2539ae8f6981630ca
Felix C. Stegerman <flx@obfusk.net>
2017-12-18 10:56:08 +0100

    + markdown, blog; ready for repos, gists

--- Makefile
+++ Makefile
@@ -1,17 +1,19 @@
-SHELL = /bin/bash
+SHELL := /bin/bash
 
-.PHONY: all build clean serve master
+.PHONY: build clean serve master
 
-all: build
-
-build:
+build: css/pygments.css
 	mkdir -p __html__
 	python3 build.py
 	cp -av css img js __html__/
 
+css/pygments.css:
+	pygmentize -S friendly -f html -a .codehilite > $@
+
 clean:
 	find -name '*~' -delete -print
 	rm -fr __html__/
+	rm -f css/pygments.css
 
 serve: build
 	cd __html__ && python3 -m http.server 8888
--- build.py
+++ build.py
@@ -1,26 +1,43 @@
 from datetime import date
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2   import Environment, FileSystemLoader, Markup, \
+                     select_autoescape
+from markdown import Markdown
 
-templates = "index".split()
+def blog_header(date, title):
+  anchor = "t_" + "_".join( "".join( c for c in s if c.isalnum() )
+                            for s in title.lower().split() )
+  return Markup(
+    """<div class="anchor" id="{0}"></div>\n"""
+    """## [{1}](#{0})\n"""
+    """<small>[{2}]</small>"""
+  .format(anchor, title, date))
+
+templates = "index blog repos gists".split()
 data      = dict(
-  title     = '/var/log/obfusk',
-  tagline   = 'hacking ⇒ ¬sleeping',
-  name      = 'Felix C. Stegerman',
-  year      = date.today().year,
-  navs      = [
-    dict(page = "index"   , link = "/"    , text = "Home"),
-    dict(page = "old-blog", link = "/old" , text = "Old Blog"),
-  ]
+  title       = "/var/log/obfusk",
+  tagline     = "hacking ⇒ ¬sleeping",
+  name        = "Felix C. Stegerman",
+  year        = date.today().year,
+  navs        = [
+    dict(page = "index"   , link = "/"          , text = "Home"),
+    dict(page = "blog"    , link = "/blog.html" , text = "Blog"),
+    dict(page = "repos"   , link = "/repos.html", text = "Repositories"),
+    dict(page = "gists"   , link = "/gists.html", text = "Gists"),
+    dict(page = "old-blog", link = "/old"       , text = "Old Blog"),
+  ],
+  blog_header = blog_header,
+  blog_sep    = Markup("""<hr class="blog_sep" />"""),
 )
 
-env = Environment(
-  loader      = FileSystemLoader('templates'),
-  autoescape  = select_autoescape()
-)
+md  = Markdown(output_format = "xhtml5",
+               extensions = "extra codehilite".split())
+env = Environment(loader = FileSystemLoader("templates"),
+                  autoescape = select_autoescape())
+env.filters["markdown"] = lambda s: Markup(md.convert(s))
 
 for t in templates:
   print("building {}.html ...".format(t))
-  template = env.get_template(t + '.html')
-  with open('__html__/{}.html'.format(t), 'w') as f:
+  template = env.get_template(t + ".html")
+  with open("__html__/{}.html".format(t), "w") as f:
     template.stream(page = t, **data).dump(f)
-    f.write("\n")   # ensure EOL; TODO: why? always?
+    f.write("\n")
