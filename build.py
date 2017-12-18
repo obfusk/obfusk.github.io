ea87ad7d3bba9710fedade1b1b81431c9098f4a7
Felix C. Stegerman <flx@obfusk.net>
2017-12-18 20:46:01 +0100

    blog + repos OK; lots of .json, gh.py; TODO: gists

--- build.py
+++ build.py
@@ -1,18 +1,18 @@
+import json
+
 from datetime import date
 from jinja2   import Environment, FileSystemLoader, Markup, \
                      select_autoescape
 from markdown import Markdown
 
-def blog_header(date, title):
-  anchor = "t_" + "_".join( "".join( c for c in s if c.isalnum() )
-                            for s in title.lower().split() )
-  return Markup(
-    """<div class="anchor" id="{0}"></div>\n"""
-    """## [{1}](#{0})\n"""
-    """<small>[{2}]</small>"""
-  .format(anchor, title, date))
+def anchor(t):
+  return "a_" + "_".join( "".join( c for c in s if c.isalnum() )
+                          for s in t.lower().split() )
 
-templates = "index blog repos gists".split()
+with open("data/repos.json") as f: repos = json.load(f)
+with open("data/gists.json") as f: gists = json.load(f)
+
+templates = "index blog repos".split()  # TODO: gists
 data      = dict(
   title       = "/var/log/obfusk",
   tagline     = "hacking ⇒ ¬sleeping",
@@ -22,11 +22,13 @@ data      = dict(
     dict(page = "index"   , link = "/"          , text = "Home"),
     dict(page = "blog"    , link = "/blog.html" , text = "Blog"),
     dict(page = "repos"   , link = "/repos.html", text = "Repositories"),
-    dict(page = "gists"   , link = "/gists.html", text = "Gists"),
+#   dict(page = "gists"   , link = "/gists.html", text = "Gists"),
     dict(page = "old-blog", link = "/old"       , text = "Old Blog"),
   ],
-  blog_header = blog_header,
-  blog_sep    = Markup("""<hr class="blog_sep" />"""),
+  ptitles     = dict(repos = "repositories"),
+  anchor      = lambda t: Markup(anchor(t)),
+  repos       = repos,
+  gists       = gists,
 )
 
 md  = Markdown(output_format = "xhtml5",
@@ -39,5 +41,4 @@ for t in templates:
   print("building {}.html ...".format(t))
   template = env.get_template(t + ".html")
   with open("__html__/{}.html".format(t), "w") as f:
-    template.stream(page = t, **data).dump(f)
-    f.write("\n")
+    template.stream(page = t, **data).dump(f); f.write("\n")
